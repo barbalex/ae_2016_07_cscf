7da7f08f0283c35389f246b34797a579cc7e9d58
Alexander Gabriel <alex@gabriel-software.ch>
2016-07-11 18:30:21 +0200

    optimize

--- src/updateNuesp.js
+++ src/updateNuesp.js
@@ -6,32 +6,29 @@ const Promise = require('bluebird')
 const taxIdsNew = require('./taxIdNew.js')
 
 module.exports = (db, objects) => {
-  const promises = taxIdsNew.map((tax) =>
-    new Promise((resolve2) => {
-      // find object
-      const object = objects.find((o) => o._id === tax.GUID)
-      if (object) {
-        if (object.Taxonomie && object.Taxonomie.Eigenschaften && object.Taxonomie.Eigenschaften['Taxonomie ID']) {
-          // update nuesp
-          object.Taxonomie.Eigenschaften['Taxonomie ID'] = tax.TaxonomieId
-          // save doc and update rev
-          db.saveAsync(object)
+  const promises = []
+  taxIdsNew.forEach((tax) => {
+    // find object
+    const object = objects.find((o) => o._id === tax.GUID)
+    if (object) {
+      if (object.Taxonomie && object.Taxonomie.Eigenschaften && object.Taxonomie.Eigenschaften['Taxonomie ID']) {
+        // update nuesp
+        object.Taxonomie.Eigenschaften['Taxonomie ID'] = tax.TaxonomieId
+        // save doc and update rev
+        const promise = db.saveAsync(object)
           .then((result) => {
             object._res = result.res
             console.log(`The Taxonomy ID of object ${object._id} was updated`)
-            resolve2(true)
           })
           .catch(() => console.log(`error saving object ${object}`))
-        } else {
-          console.log(`object ${object} did not have object.Taxonomie.Eigenschaften['Taxonomie ID']`)
-          resolve2(true)
-        }
+        promises.push(promise)
       } else {
-        console.log(`no object found for GUID ${tax.GUID}`)
-        resolve2(true)
+        console.log(`object ${object} did not have object.Taxonomie.Eigenschaften['Taxonomie ID']`)
       }
-    })
-  )
+    } else {
+      console.log(`no object found for GUID ${tax.GUID}`)
+    }
+  })
 
   return Promise.map(promises, { concurrency: 1 })
 }
