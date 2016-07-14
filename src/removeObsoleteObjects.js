58cfe7fec875a46c181b14743001d971ce0ad638
Alexander Gabriel <alex@gabriel-software.ch>
2016-07-14 17:21:30 +0200

    use bulk save

--- src/removeObsoleteObjects.js
+++ src/removeObsoleteObjects.js
@@ -1,35 +1,41 @@
 /* eslint no-console:0, func-names:0, prefer-arrow-callback:0 */
 'use strict'
 
-const async = require('async')
 const idsToKeep = require('./idsToKeep.js')
 
-module.exports = (db, objects, cscf) => {
-  const q = async.queue(function (o, callback) {
-    db.remove(o._id, o._rev, function () {
-      // seems that async needs this callback
-      callback()
+module.exports = (db, objects, cscf) =>
+  new Promise((resolve, reject) => {
+    const objectsToDelete = []
+    const cscfTaxIds = cscf.map((c) => c.TaxonomieId)
+
+    objects.forEach((o) => {
+      if (
+        !idsToKeep.includes(o._id) &&
+        (
+          o.Taxonomie &&
+          o.Taxonomie.Eigenschaften &&
+          o.Taxonomie.Eigenschaften['Taxonomie ID'] &&
+          o.Taxonomie.Eigenschaften['Taxonomie ID'] < 1000000 &&
+          !cscfTaxIds.includes(o.Taxonomie.Eigenschaften['Taxonomie ID'])
+        )
+      ) {
+        // this object is obsolete
+        o._deleted = true // eslint-disable-line no-underscore-dangle
+        objectsToDelete.push(o)
+      }
     })
-  })
-  q.drain = function () {
-    console.log('obsolete objects removed')
-  }
-  const cscfTaxIds = cscf.map((c) => c.TaxonomieId)
-  objects.forEach((o) => {
-    if (
-      !idsToKeep.includes(o._id) &&
-      (
-        o.Taxonomie &&
-        o.Taxonomie.Eigenschaften &&
-        o.Taxonomie.Eigenschaften['Taxonomie ID'] &&
-        o.Taxonomie.Eigenschaften['Taxonomie ID'] < 1000000 &&
-        !cscfTaxIds.includes(o.Taxonomie.Eigenschaften['Taxonomie ID'])
-      )
-    ) {
-      // this object is obsolete
-      q.push(o, function () {
-        // do nothing
-      })
+
+    if (objectsToDelete.length === 0) {
+      console.log('0 obsolete objects removed')
+      return resolve()
     }
+    db.saveAsync(objectsToDelete)
+      .then(() => {
+        console.log(`${objectsToDelete.length} obsolete objects removed`)
+        resolve()
+      })
+      .catch((error) => {
+        console.error('removeObsoleteObjects error:', error)
+        reject()
+      })
   })
-}
